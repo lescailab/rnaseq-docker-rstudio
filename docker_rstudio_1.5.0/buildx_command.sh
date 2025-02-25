bf61a8545fdf0b366f8f73e3cb9dc14743d437e9
Lorenzo Sola <47034913+LorenzoS96@users.noreply.github.com>
2025-02-25 13:47:08 +0000

    docker release 1.5.0

---
+++ docker_rstudio_1.5.0/buildx_command.sh
@@ -0,0 +1,14 @@
+# docker buildx create --platform "linux/amd64" --name build --use
+
+docker buildx build \
+--label org.opencontainers.image.title=rnaseq-docker-rstudio \
+--label org.opencontainers.image.description='container for rna-seq analysis with rstudio and deseq2' \
+--label org.opencontainers.image.url=https://github.com/lescailab/rnaseq-docker-rstudio \
+--label org.opencontainers.image.source=https://github.com/lescailab/rnaseq-docker-rstudio \
+--label org.opencontainers.image.version=1.5.0 \
+--label org.opencontainers.image.created=$(date -u +"%Y-%m-%dT%H:%M:%S.%3NZ") \
+--label org.opencontainers.image.licenses=MIT \
+--platform linux/amd64 \
+--tag ghcr.io/lescailab/rnaseq-docker-rstudio:1.5.0 \
+--tag ghcr.io/lescailab/rnaseq-docker-rstudio:latest \
+--push .
